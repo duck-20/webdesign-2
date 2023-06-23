aaff0002b6328f658ef93207c49f1a67abeedddf
duck-20 <pakhrinayush56@gmail.com>
2023-06-23 17:09:36 +0545

    falsy and adding in array in html using targer

---
+++ js/falsy.js
@@ -0,0 +1,15 @@
+/* 
+falsy Value
+    -null
+    -undefined
+    -0
+    -false
+    -NaN
+    -"" // empty string
+*/
+if(0){
+  console.log("true condition")
+}
+else{
+  console.log("false condition")
+}
